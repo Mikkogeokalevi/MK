beb41e76a26dc08569845561bc232bd718b2eb71
Mikkogeokalevi <mikkogeokalevi@gmail.com>
2025-05-27 19:19:03 +0300

    Update puzzle_data.js

--- puzzle_data.js
+++ puzzle_data.js
@@ -8,31 +8,31 @@ const puzzleSetup = {
             // Vaihe 1 (indeksi 0)
             expectedInput: "salasana", // Mitä käyttäjän pitää kirjoittaa
             responseMessage: "Salasana on väärin.", // Viesti oikeasta syötteestä
-            promptForNext: "...:" // Kehote seuraavaa syötettä varten
+            promptForNext: "Nyt ei mennyt oikein:" // Kehote seuraavaa syötettä varten
         },
         {
             // Vaihe 2 (indeksi 1)
             expectedInput: "väärin",
             responseMessage: "Kokeile uudestaan",
-            promptForNext: "..."
+            promptForNext: "Oletko nyt aivan varma tuosta?"
         },
         {
             // Vaihe 3 (indeksi 2)
             expectedInput: "uudestaan",
             responseMessage: "Salasana on edelleen väärin",
-            promptForNext: "..."
+            promptForNext: "Et taida muistaa?"
         },
         {
             // Vaihe 4 (indeksi 3) - UUSI VAIHE
             expectedInput: "edelleen väärin", // Esimerkki uudesta vaiheesta
             responseMessage: "Melkein perillä, anna seuraava:",
-            promptForNext: "..."
+            promptForNext: "Miten tämä on nyt näin vaikeaa?"
         },
         {
             // Vaihe 5 (indeksi 4) - UUSI VAIHE
             expectedInput: "seuraava", // Esimerkki uudesta vaiheesta
             responseMessage: "Kirjoita viimeinen sana, ennen ratkaisua:",
-            promptForNext: "..."
+            promptForNext: "Jokohan nyt osuisi oikeaan"
         },
         {
             // Vaihe 6 (indeksi 5) - Alkuperäinen viimeinen "oikea"
